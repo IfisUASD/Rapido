1ec6814de929570f2a4415d5c3077175c005e774
Fabio18 <fabioleninpimentel@gmail.com>
2021-11-02 10:45:33 -0400

    first commit

---
+++ rapido.sh
@@ -0,0 +1,471 @@
+#!/bin/bash
+
+cd k=2
+
+cd L64
+cd K2L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K2L96
+simple main 
+cd ..
+cd ..
+
+cd L128
+cd K2L128
+simple main
+cd ..
+cd ..
+
+cd L256
+cd K2L256
+simple main
+cd ..
+cd ..
+
+cd L512
+cd K2L512
+simple main
+cd ..
+cd ..
+
+cd L640
+cd K2L640
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=3
+ 
+cd L64
+cd K3L64
+simple main 
+cd ..
+cd ..
+
+cd L96
+cd K3L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K3L128
+simple main
+cd ..
+cd ..
+
+cd L256
+cd K3L256
+simple main
+cd ..
+cd ..
+
+cd L512
+cd K3L512
+simple main
+cd ..
+cd ..
+
+cd L640
+cd K3L640
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=4
+
+cd L64
+cd K4L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K4L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K4L128
+simple main 
+cd ..
+cd ..
+
+cd L256
+cd K4L256
+simple main
+cd ..
+cd ..
+
+cd L512
+cd K4L512
+simple main
+cd ..
+cd ..
+
+cd L640
+cd K4L640
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=5
+
+cd L64
+cd K5L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K5L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K5L128
+simple main
+cd ..
+cd ..
+
+cd L256
+cd K5L256
+simple main
+cd ..
+cd ..
+
+cd L512
+cd K5L512
+simple main
+cd ..
+cd ..
+
+cd L640
+cd K5L640
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=6
+
+cd L64
+cd K6L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K6L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K6L128
+simple main
+cd ..
+cd ..
+
+cd L256
+cd K6L256
+simple main
+cd ..
+cd ..
+
+cd L512
+cd K6L512
+simple main
+cd ..
+cd ..
+
+cd L640
+cd K6L640
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=8 
+
+cd L64
+cd K8L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K8L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K8L128
+simple main
+cd ..
+cd ..
+
+cd L256
+cd K8L256
+simple main
+cd ..
+cd ..
+
+cd L512
+cd K8L512
+simple main
+cd ..
+cd ..
+
+cd L640
+cd K8L640
+simple main
+cd ..
+cd ..
+ 
+cd ..
+
+cd k=10
+
+cd L64
+cd K10L64
+simple main 
+cd ..
+cd ..
+
+cd L96
+cd K10L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K10L128
+simple main
+cd ..
+cd ..
+
+cd L256
+cd K10L256
+simple main
+cd ..
+cd ..
+
+cd L512
+cd K10L512
+simple main
+cd ..
+cd ..
+
+cd L640
+cd K10L640
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=12
+
+cd L64
+cd K12L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K12L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K12L128
+simple main
+cd ..
+cd ..
+
+cd L256
+cd K12L256
+simple main 
+cd ..
+cd ..
+
+cd L512
+cd K12L512
+simple main 
+cd ..
+cd ..
+
+cd L640
+cd K12L640
+cd ..
+cd ..
+
+cd ..
+
+cd k=16
+
+cd L64
+cd K16L54
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K16L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K16L128
+simple main
+cd ..
+cd ..
+
+cd L256
+cd K16L256
+simple main
+cd ..
+cd ..
+
+cd L512
+cd K16L512
+simple main
+cd ..
+cd ..
+
+cd L640
+cd K16L620
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=24
+
+cd L48
+cd K24L48
+simple main
+cd ..
+cd ..
+
+cd L64
+cd K24L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K24L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K24L128
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=32
+
+cd L48
+cd K32L48
+simple main
+cd ..
+cd ..
+
+cd L64
+cd K32L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K32L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K32L128
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=48
+
+cd L48
+cd K48L48
+simple main
+cd ..
+cd ..
+
+cd L64
+cd K48L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K48L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K48L128
+simple main
+cd ..
+cd ..
+
+cd ..
+
+cd k=64
+
+cd L48
+cd K64L48
+simple main
+cd ..
+cd ..
+
+cd L64
+cd K64L64
+simple main
+cd ..
+cd ..
+
+cd L96
+cd K64L96
+simple main
+cd ..
+cd ..
+
+cd L128
+cd K64L128
+simple main
+cd ..
+cd ..
+
